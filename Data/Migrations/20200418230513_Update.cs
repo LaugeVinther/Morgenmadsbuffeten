f753a30e5d1b4aefda2527302a1c0c6d622d4181
megsgaard <mathias.egsgaard@post.au.dk>
2020-04-19 01:08:13 +0200

    Update database

---
+++ Data/Migrations/20200418230513_Update.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Morgenmadsbuffeten.Data.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    ReservationId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumberOfAdults = table.Column<int>(nullable: false),
+                    NumberOfChildren = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservation", x => x.ReservationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservation");
+        }
+    }
+}
